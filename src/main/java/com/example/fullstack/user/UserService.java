a2fdb7115f4a4bbbfc162086d75c8a47d5c62465
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2024-09-22 23:00:51 +0100

    start authentication and authorization implementation

--- src/main/java/com/example/fullstack/user/UserService.java
+++ src/main/java/com/example/fullstack/user/UserService.java
@@ -1,62 +1,62 @@
 package com.example.fullstack.user;
 
-import java.util.List;
-
-import org.hibernate.ObjectNotFoundException;
-
 import com.example.fullstack.project.Project;
 import com.example.fullstack.task.Task;
-
 import io.quarkus.elytron.security.common.BcryptUtil;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
+import org.hibernate.ObjectNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 @ApplicationScoped
 public class UserService {
 
-    public Uni<User> findById(long id) {
-        return User.<User>findById(id)
-                .onItem()
-                .ifNull()
-                .failWith(() -> new ObjectNotFoundException(id, "User"));
-    }
+  private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
-    public Uni<User> findByName(String name) {
-        return User.find("name", name).firstResult();
-    }
+  public Uni<User> findById(long id) {
+    return User.<User>findById(id).onItem().ifNull()
+        .failWith(() -> new ObjectNotFoundException(id, "User"));
+  }
 
-    public Uni<List<User>> list() {
-        return User.listAll();
-    }
+  public Uni<User> findByName(String name) {
+    return User.find("name", name).firstResult();
+  }
 
-    @Transactional
-    public Uni<User> create(User user) {
-        user.password = BcryptUtil.bcryptHash(user.password);
+  public Uni<List<User>> list() {
+    return User.listAll();
+  }
 
-        return user.persistAndFlush();
-    }
+  @Transactional
+  public Uni<User> create(User user) {
+    user.password = BcryptUtil.bcryptHash(user.password);
 
-    @Transactional
-    public Uni<User> update(User user) {
-        return findById(user.id)
-                .chain(() -> User.getSession())
-                .chain(sessionUser -> sessionUser.merge(user));
-    }
+    return user.persistAndFlush();
+  }
 
-    @Transactional
-    public Uni<Void> delete(long id) {
-        return findById(id)
-                .chain(user -> Uni.combine().all().unis(
-                        Task.delete("user.id", user.id),
-                        Project.delete("user.id", user.id))
-                        .asTuple()
-                        .chain(user::delete));
-    }
+  @Transactional
+  public Uni<User> update(User user) {
+    return findById(user.id).chain(() -> User.getSession())
+        .chain(sessionUser -> sessionUser.merge(user));
+  }
 
-    public Uni<User> getCurrentUser() {
-        // TODO: replace implementation with the security
+  @Transactional
+  public Uni<Void> delete(long id) {
+    return findById(id).chain(user -> Uni.combine().all()
+        .unis(Task.delete("user.id", user.id), Project.delete("user.id", user.id)).asTuple()
+        .chain(user::delete));
+  }
 
-        return User.find("order by ID").firstResult();
-    }
+  public Uni<User> getCurrentUser() {
+    // TODO: replace implementation with the security
+
+    return User.find("order by ID").firstResult();
+  }
+
+  public static boolean matches(User user, String password) {
+    return BcryptUtil.matches(password, user.password);
+  }
 }
